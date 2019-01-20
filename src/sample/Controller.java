5ced2bc0407f2b13b01febbf6ba1ab6b1b3ba66a
Vasa <dolicvasilije@gmail.com>
2019-01-20 12:38:43 +0100

    Login Form

---
+++ src/sample/Controller.java
@@ -0,0 +1,13 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.scene.input.MouseEvent;
+
+public class Controller {
+    public void login(ActionEvent actionEvent) {
+    }
+
+    public void workers(ActionEvent actionEvent) {
+        System.out.println("Workers");
+    }
+}
